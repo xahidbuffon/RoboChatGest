8e417ab8b1d94e1c61bfd7ad159fcffa6f60734f
Md Jahidul Islam <xahid@cs-stargazer-umh.cs.umn.edu>
2018-12-20 00:48:16 -0600

    robochat gest tested

--- libs/robo_chat_gester.py
+++ libs/robo_chat_gester.py
@@ -25,7 +25,7 @@ class RoboChatGest_pipeline:
         """ 
            Class for generating hand-gesture based instructions using robo_chat_gest 
         """
-	def __init__(self, real_time=False):
+	def __init__(self, menue_mode=False):
                 # instance for hand gesture recognition
 		self.gest_rec = HandGestRecognition()
                 # we have 10 classes (see the paper ieeexplore.ieee.org/document/8543168)
@@ -35,17 +35,12 @@ class RoboChatGest_pipeline:
 		self.ins = InstructionGeneration(self.classes)
                 # flags for Aqua menue selection
 		self.men_sel = MenueSelection(self.classes)
-		self.menue_map = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5}
 
-		self.frame_no = 0
-                
-                with open('model_data/robo_chat_gest_params.yaml', 'r') as run_param_f:
-                        run_param_dict = yaml.load(run_param_f)
-		self.menue_mode = run_param_dict['set_Menue_mode_']
-		self.robo_gest_mode = run_param_dict['set_RoboGest_mode_']
-		self.bench_test = run_param_dict['set_Bench_Test_']
-		self.publish_image = run_param_dict['set_Publish_Image_']
-		self.use_single_hand = run_param_dict['use_Single_Hand_Gestures_only'] 
+                if menue_mode: 
+			self.menue_mode, self.robo_gest_mode = True, False
+		else:
+			self.menue_mode, self.robo_gest_mode = False, True    
+		self.use_single_hand = False
 			
 
 	def ImageProcessor(self, img, vizualize=False, wait_time=1):
@@ -58,34 +53,26 @@ class RoboChatGest_pipeline:
                 """
                 # get the tokens (and the bounding boxes for vizualization)
 		left_token, left_box, right_token, right_box, success_ = self.gest_rec.Get_gest(img, self.use_single_hand)
-		print ("Hand gestures detected: ({0}, {1})".format(self.obj_classes[right_token], self.obj_classes[left_token]))
+		print ("Hand gestures detected: ({0}, {1})".format(right_token, left_token))
 
 		if success_:
                         if vizualize:
                         	localised_objs = [(left_token, left_box), (right_token, right_box)]
                         	img = draw_boxes_and_labels(img, localised_objs, self.obj_classes)
-
-			# ROBO_GEST mode
-			if self.robo_gest_mode:
-                                # reverse left and right since camera(left, right) == person(right, left)
-                                #  then pass it to generate instruction
+			
+			if self.robo_gest_mode: # ROBO_GEST mode
+                                # camera(left, right) == person(right, left)
 				get_token, done_ = self.ins.decode(right_token, left_token)
-				print (get_token, done_)
-				if done_:
-					#>> Here we perform the operations/service calls to execute it
-                                        print 
-                                        print ("*** Decoded Instruction: {0}".format(get_token))
-                                        print
+			elif self.menue_mode: # Menue Selection mode
+				get_token, done_ = self.men_sel.decode(right_token, left_token)
+                        else: pass
 
-			# For Menue Selection only
-			if self.menue_mode:
-				men_ins_, men_done_ = self.men_sel.decode(right_token, left_token)
-                                if men_ins_ != '': 
-	                                img = write_instructions(img, men_ins_)
-				if men_done_:
+                        if (not get_token or get_token != ''): 
+	                        img = write_instructions(img, get_token)
+				if done_:
 					#>> Here we perform the operations/service calls to change the menue
                                         print 
-                                        print ("Decoded Instruction: {0}".format(men_ins_))
+                                        print ("Decoded Instruction: {0}".format(get_token))
                                         print
 					
 		self.vizualize_img(img, vizualize, wait_time)
